9a25bf8756410c5409ebded82899d8d4b896cfab
MrDadone <hagiudavid971@gmail.com>
2026-02-01 01:41:43 +0000

    Ui improvements

--- frontend/src/elements/Notification.tsx
+++ frontend/src/elements/Notification.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from 'react';
 
 const Notification = forwardRef<HTMLDivElement, NotificationProps>(({ className, ...rest }, ref) => {
   return (
-    <div className="pt-2 px-12">
+    <div className='pt-2 px-12'>
       <MantineNotification ref={ref} className={className} radius='md' withCloseButton={false} {...rest} />
     </div>
   );
