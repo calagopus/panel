bab61db1bfa1f4ba7460e30b546e7129e3e9d04e
cst1x <costytalent981@proton.me>
2026-07-22 03:52:49 +0300

    Update AdminServerBackupRow.tsx

--- frontend/src/pages/admin/servers/backups/AdminServerBackupRow.tsx
+++ frontend/src/pages/admin/servers/backups/AdminServerBackupRow.tsx
@@ -45,7 +45,7 @@ export default function AdminServerBackupRow({ backup }: { backup: z.infer<typeo
     downloadNodeBackup(backup.node.uuid, backup.uuid, archiveFormat)
       .then(({ url }) => {
         addToast(t('pages.admin.nodes.tabs.backups.page.toast.downloadStarted', {}), 'success');
-        window.open(url, '_blank');
+        window.location.href = url;
       })
       .catch((msg) => {
         addToast(httpErrorToHuman(msg), 'error');
